4f1d1c236cd15422db5930d7267d325d3b790ea2
Stephen Tersoo <stephentersoo17@gmail.com>
2025-02-11 23:34:37 +0100

    Added Intro Text

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,21 @@ export default function Home() {
     return (
         <div className="  ">
             <HeroSection />
+            <div className="w-[90%] mx-auto md:w-[80%]">
+                <p className="w-full mt-[3rem] text-[16px] md:text-[18px]">
+                    As an entrepreneur, protecting your business interests is
+                    paramount, and registering your business not only lends it
+                    legal credibility but also serves as a vital shield against
+                    potential financial mismanagement, including the risk of
+                    funds being misappropriated by a business partner. When you
+                    officially register your business, you establish a clear
+                    legal framework, outlining ownership structures,
+                    responsibilities, and financial obligations. This
+                    documentation not only solidifies your position as a
+                    legitimate entity but also provides a transparent record of
+                    financial agreements and expectations.
+                </p>
+            </div>
             <ServicesHomePage />
             <OurTeam />
         </div>
